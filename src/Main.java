98590c7d1470e51fb41dbb1673428ac60ccfb747
Daniel <danielsouzanovo@gmail.com>
2025-07-09 20:38:05 -0300

    tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import vetor.Vetor;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Vetor vetor = new Vetor<>(5);
+        vetor.adicionar("A");
+        vetor.adicionar("B");
+        vetor.adicionar("C");
+        vetor.adicionar("D");
+        vetor.adicionar("E");
+
+        System.out.println("Vetor atual: "+vetor);
+
+        vetor.remover("B");
+
+        System.out.println("Vetor após remover: "+vetor);
     }
 }
